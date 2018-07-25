2beda236711051d03328b07ce1bcc2bfcc507aa2
sjc11221 <stevejchang@gmail.com>
2018-07-25 15:08:17 -0400

    Create r10k.pp

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,13 @@
+class profile::r10k {
+  class {'r10k':
+    remote => 'https://github.com/sjc11221/control_repo',
+   }
+   class {'r10k::webhook::config' : 
+      use_mcollective => false,
+      enable_ssl => false,
+   }   
+   class {'r10k::webhook' :
+    user => 'root',
+    group => 'root',
+   }
+ }
